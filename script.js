1d7d0d6c05d8416e5306a9c88c6c70d87494a381
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-06-26 20:32:02 +0100

    Added event listeners

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+/** Variables **/
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
 function calculate() {
-  
+  console.log('RAN');
 }
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_one.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
 
 calculate();
